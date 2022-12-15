53bd8200ca3bf2b19ba2acc8bf8b640b22a8a787
maneesha_lakshani <maneeshalakshani9@gmail.com>
2022-12-15 13:32:35 +0530

    fix: service class format

--- src/main/java/com/spring/demo/service/ClerkService.java
+++ src/main/java/com/spring/demo/service/ClerkService.java
@@ -3,51 +3,22 @@ package com.spring.demo.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.spring.demo.domain.Clerk1;
-import com.spring.demo.repository.Cler1kRepository;
 
-@Component
-public class ClerkService {
-
-    @Autowired
-    private Cler1kRepository clerkRepository;
+public interface ClerkService {
 
     //save clerk
-    public Clerk1 addClerk(Clerk1 clerk){
-        return this.clerkRepository.save(clerk);
-    }    
+    public Clerk1 addClerk(Clerk1 clerk);   
 
     //get a clerk
-    public Optional<Clerk1> getAClerk(long clerkID){
-        // return clerkRepository.findByClerkID(clerkID).get(0);
-        return clerkRepository.findById(clerkID);
-    }
+    public Optional<Clerk1> getAClerk(long clerkID);
 
     //get All Clerks
-    public List<Clerk1> getAllClerks(){
-        return clerkRepository.findAll();
-    }
+    public List<Clerk1> getAllClerks();
 
     //update clerk
-    public Clerk1 updateClerk(long clerkID, Clerk1 clerk){
-        Clerk1 returnClerk = clerkRepository.findByClerkID(clerkID).get(0);
-        if(returnClerk != null){
-            clerk.setClerkID(clerkID);
-            return clerkRepository.save(clerk);
-        }
-        return null;
-    }
+    public Clerk1 updateClerk(long clerkID, Clerk1 clerk);
 
     //delete clerk
-    public boolean deleteClerk(long clerkID){
-        try{
-            clerkRepository.deleteById(clerkID);
-            return true;
-        }catch (Exception e){
-            return false;
-        }
-    }
+    public boolean deleteClerk(long clerkID);
 }
