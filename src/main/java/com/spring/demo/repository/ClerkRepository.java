43068bcf53294f78bbdece9d4a29dfed61938f5d
maneesha_lakshani <maneeshalakshani9@gmail.com>
2022-12-16 10:23:45 +0530

    fix: rename Clerk files & classes

--- src/main/java/com/spring/demo/repository/ClerkRepository.java
+++ src/main/java/com/spring/demo/repository/ClerkRepository.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.spring.demo.domain.Clerk1;
+import com.spring.demo.domain.Clerk;
 
 @Repository("clerkRepository")
-public interface Cler1kRepository extends JpaRepository<Clerk1, Long>{
-    List<Clerk1> findByClerkID(long clerkID);
+public interface ClerkRepository extends JpaRepository<Clerk, Long>{
+    List<Clerk> findByClerkID(long clerkID);
 }
