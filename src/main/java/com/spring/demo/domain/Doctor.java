af26209abf519ecfd99a03dd00c805996784ad76
Tharushka Rupasinghe <tharushka.rupasinghe@hcl.com>
2022-12-22 14:23:20 +0000

    doctor validation

--- src/main/java/com/spring/demo/domain/Doctor.java
+++ src/main/java/com/spring/demo/domain/Doctor.java
@@ -6,6 +6,10 @@ import java.util.Set;
 
 import jakarta.persistence.*;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 @Entity
 @Table(name = "doctor")
 public class Doctor {
@@ -14,21 +18,37 @@ public class Doctor {
 	private long doctorId;
 
 	@Column(name = "FIRSTNAME")
+	@NotBlank(message = "Mandatory")
+    @NotNull
+	@Size(min = 3, max = 20, message="Characters should be between 3 and 20")
 	private String firstName;
 
 	@Column(name = "LASTNAME")
+	@NotBlank(message = "Mandatory")
+    @NotNull
+	@Size(min = 3, max = 20, message="Characters should be between 3 and 20")
 	private String lastName;
 
 	@Column(name = "NICNUMBER")
+	@NotBlank(message = "Mandatory")
+    @NotNull
+	@Size(min = 9, max = 12, message="Characters should be between 9 and 12")
 	private String nicNumber;
 
 	@Column(name = "GENDER")
+	@NotBlank(message = "Mandatory")
+    @NotNull
 	private String gender;
 
 	@Column(name = "CONTACTNUMBER")
+	@NotBlank(message = "Mandatory")
+    @NotNull
+	@Size(min = 10, max = 12, message="Characters should be between 10 and 12")
 	private String contactNumber;
 
 	@Column(name = "SPECIALIZATION")
+	@NotBlank(message = "Mandatory")
+    @NotNull
 	private String specialization;
 
 	@OneToOne(mappedBy = "doctor")
