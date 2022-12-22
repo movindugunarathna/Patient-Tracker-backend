9174fbf475c41e88d140208017411d670d93e13b
Tharushka Rupasinghe <tharushka.rupasinghe@hcl.com>
2022-12-22 09:58:57 +0000

    DocControll with log and excpetion

--- src/main/java/com/spring/demo/controller/DoctorController.java
+++ src/main/java/com/spring/demo/controller/DoctorController.java
@@ -13,7 +13,72 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value = "/doctor")
 public class DoctorController {
+
+     @Autowired
+    private DoctorRepository doctorRepo;
+    
     @Autowired
+    private DoctorService doctorService;
+    
+    static Logger logger = Logger.getLogger(DoctorController.class.getName());
+    
+    @RequestMapping(value="/list")
+    public ResponseEntity<List<Doctor>>getDoctor(){
+    	logger.info("Inside class!!!!!!!! DoctorController, method!!!! : getDoctorsList");
+    	return new ResponseEntity<List<Doctor>>(doctorService.getDoctor(),HttpStatus.OK);
+    }
+    
+    @RequestMapping(value="/add", method = RequestMethod.POST)
+    public ResponseEntity<Object>createDoctor(@Valid @RequestBody Doctor doctor) {
+    	logger.info("Inside class!!!!!!!! DoctorController, method!!!! : createDoctor");
+    		//Doctor doc = doctorRepo.save(doc)
+    	doctorService.createDoctor(doctor);
+    	return new ResponseEntity<>("Doctor added successfully", HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/list/{doctorID}", method=RequestMethod.GET)
+    public ResponseEntity<Doctor> getDoctorById(@PathVariable("doctorID") Long doctorID) {
+        logger.info("Inside Class!!!!!!!! DoctorController, method!!!!! :  getDoctorById");
+        
+        Doctor doc= doctorRepo.findById(doctorID).orElseThrow(()-> new DoctorNotFoundException());
+       // if(!doctorService.containsKey(doctorId)) throw new DoctorNotFoundException(;)
+        //return new ResponseEntity<Doctor>(doctorService.getDoctorById(doctorID), HttpStatus.OK);
+        return ResponseEntity.ok().body(doc);
+    }
+    
+    @RequestMapping(value = "/delete/{doctorID}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteDoctor(@PathVariable("doctorID") Long doctorID) {
+        logger.info("Inside class!!!!!!!! DoctorController, method!!!! : delete");
+        
+        Doctor doc = doctorRepo.findById(doctorID).orElseThrow(()-> new DoctorNotFoundException());
+        		
+        doctorService.deleteDoctor(doctorID);
+        //return new ResponseEntity<>("Doctor deleted successsfully", HttpStatus.OK);
+        
+        return ResponseEntity.ok().body("Doctor with Id "+doctorID+" DELETED");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*@Autowired
     private DoctorServiceImpl doctorServiceImpl;
 
     @GetMapping(value = "/list")
@@ -45,6 +110,6 @@ public class DoctorController {
     public Doctor updateDoctor(@RequestBody Doctor updateDoctor, @PathVariable Long id) {
         return doctorServiceImpl.updateDoctor(updateDoctor, id);
     }
-    
+    */
 
 }
